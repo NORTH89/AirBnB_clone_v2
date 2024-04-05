c3e474e2a5ed73aefb0302f50e61b9a84e0a3feb
NORTH89 <nordintata@gmail.com>
2024-04-05 16:55:06 -0700

    update code

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,65 +1,30 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to web servers.
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
 
-from datetime import datetime
-from fabric.api import local, put, run, env
-import os
-
-env.hosts = ["54.164.136.249", "34.229.69.209"]
-env.user = "ubuntu"
-
-
-def do_pack():
-    """Pack web_static into .tgz archive."""
-
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_path = "versions/web_static_{}.tgz".format(date)
-
-    if local("tar -cvzf {} web_static".format(archive_path)).succeeded:
-        return archive_path
-    return None
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['54.197.97.2', '34.229.186.37']
 
 
 def do_deploy(archive_path):
-    """Deploy an archive to web servers."""
-
-    if not os.path.exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
     try:
-        archive_name = os.path.basename(archive_path)
-        base_name = os.path.splitext(archive_name)[0]
-        release_dir = "/data/web_static/releases/{}".format(base_name)
-
-        # Transfer archive
-        put(archive_path, "/tmp/")
-
-        # Create target directory
-        run("sudo mkdir -p {}".format(release_dir))
-
-        # Unpack archive
-        run("sudo tar -xzf /tmp/{} -C {}".format(archive_name, release_dir))
-
-        # Remove archive
-        run("sudo rm /tmp/{}".format(archive_name))
-
-        # Move unpacked files from web_static to release dir
-        run("sudo mv {}/web_static/* {}/".format(release_dir, release_dir))
-
-        # Remove web_static folder in release dir
-        run("sudo rm -rf {}/web_static".format(release_dir))
-
-        # Remove current symlink
-        run("sudo rm -rf /data/web_static/current")
-
-        # Create new symlink
-        run("sudo ln -s {} /data/web_static/current".format(release_dir))
-
-        print("New version deployed!")
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception as e:
-        print("Error:", e)
+    except:
         return False
